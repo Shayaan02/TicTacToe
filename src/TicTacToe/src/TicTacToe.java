d2823dcb7d0cf0ae3ec5b109ec5b7ebb3a1423db
Shayaan02 <shayaan.ahmed@gritacademy.se>
2023-11-21 16:11:20 +0100

    interface

--- src/TicTacToe/src/TicTacToe.java
+++ src/TicTacToe/src/TicTacToe.java
@@ -18,7 +18,15 @@ boolean player1_turn;
         frame.getContentPane().setBackground(new Color(50,50,50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free", Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic-Tac-Toe");
+        textfield.setOpaque(true);
 
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
     }
 public void actionPreformed (ActionEvent e){
 
